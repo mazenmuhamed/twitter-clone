a3721889f9bb1f10c2c46eeef6bc78c5439b287a
Mazen Mohamed <mazenmohamed404@hotmail.com>
2022-10-23 21:52:22 +0200

    Fix bug

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -92,7 +92,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         // Add avatar placeholder
         updateProfile(credential.user, {
           displayName: name,
-          photoURL: `https://scontent.fcai21-4.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?_nc_cat=1&ccb=1-7&_nc_sid=7206a8&_nc_ohc=jvAgaOmK2RAAX_C1VOX&_nc_ht=scontent.fcai21-4.fna&oh=00_AT8UMjq-IkxcIrWzP7Ep9HQ1twyvG4vUcz-8XggW0sVtoQ&oe=633D2A78`,
+          photoURL: `https://scontent.fcai21-4.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?_nc_cat=1&ccb=1-7&_nc_sid=7206a8&_nc_ohc=1Rph2yqJK04AX93nY3X&_nc_ht=scontent.fcai21-4.fna&oh=00_AT9bV_N5wYF1A1VhOmmyPq-a0psSTKWejeuiH3F5Ccdpew&oe=63787DF8`,
         }).then(() => {
           // Save user with other users in firestore
           setDoc(doc(db, 'users', credential.user.uid), {
